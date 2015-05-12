b7b4d015624e092338fba3ad30d3761106593bc5
Ian Flaherty <ianrocket@gmail.com>
2015-05-12 13:55:27 -0700

    adding ship

---
+++ src/Ship.java
@@ -0,0 +1,20 @@
+public class Ship {
+   int row;
+   int col;
+   int direction; 
+   /* 0
+    3   1
+      2
+            */
+   int size;
+   
+   
+   public Ship (){
+      
+   }
+   
+   
+   
+    
+}  
+   
