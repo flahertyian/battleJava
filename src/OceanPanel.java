57d883fa49224dcc0d862fb0af6d3d9891a541d6
Ian Flaherty <ianrocket@gmail.com>
2015-05-27 17:14:06 -0700

    set the layout of Frame to Grid layout

--- src/OceanPanel.java
+++ src/OceanPanel.java
@@ -1,6 +1,8 @@
-import javax.swing.JPanel
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event;
 
-public class OceanPanel extends JPanel{
+public class OceanPanel extends JPanel implements ActionListener{
     private int sizeX;
     private int sizeY;
     private BufferedImage topIm;
@@ -15,15 +17,23 @@ public class OceanPanel extends JPanel{
 
   private void drawBoard(Graphics2D g2d){
 
-    g2d.setColor(Color.BLACK);
-    g2d.fillRect(0,0,sizeX,sizeX);
-    g2d.setColor(Color.BLUE);
-    for(int r = 0; r < sizeX; r ++){
-      for(int c = 0; c < sizeY; c++) {
+
+    // g2d.setColor(Color.BLACK);
+    // g2d.fillRect(0,0,sizeX,sizeX);
+    // g2d.setColor(Color.BLUE);
+    // for(int r = 0; r < sizeX; r ++){
+    //   for(int c = 0; c < sizeY; c++) {
 
         //the drawing of the cubes to the image
 
-      }
-    }
+    //   }
+    // }
   }
+  private void actionPreformed(ActionEvent event){
+
+
+
+  }
+
+
 }
