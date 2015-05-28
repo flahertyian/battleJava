2389bc3fb7ade7b87790aa9a4d9025034059d4cf
Ian Flaherty <ianrocket@gmail.com>
2015-05-27 21:19:58 -0700

    made minor changes to make both test and ocean class runnable

--- src/OceanPanel.java
+++ src/OceanPanel.java
@@ -5,36 +5,32 @@ import java.awt.event.ActionListener;
 
 
 public class OceanPanel extends JPanel implements ActionListener{
-   private int sizeX;
-   private int sizeY;
-  
-   public OceanPanel(int sizeX, int sizeY){
-      this.sizeY = sizeY;
-      this.sizeX = sizeX;
+   private int sizeX
+
+   public OceanPanel(int size){
+      this.size = size;
       drawBoard();
    }
 
    private void drawBoard(){
-       for(int i = 0; i<sizeY; i++){
-          for(int j =0; j<sizeX; j++){
+          for(int j =0; j<size; j++){
             super.add(new JButton("1"));
           }
-       }
       // g2d.setColor(Color.BLACK);
     // g2d.fillRect(0,0,sizeX,sizeX);
     // g2d.setColor(Color.BLUE);
     // for(int r = 0; r < sizeX; r ++){
     //   for(int c = 0; c < sizeY; c++) {
-   
+
         //the drawing of the cubes to the image
-   
+
     //   }
     // }
    }
    public void actionPerformed(ActionEvent event){
-   
-   
-   
+
+
+
    }
 
 
--- src/battleJavaTester.java
+++ src/battleJavaTester.java
@@ -5,15 +5,15 @@ import javax.swing.JButton;
 public class battleJavaTester {
    public static void main (String [] args){
       JFrame frame = new JFrame();
-      frame.setLayout(new GridLayout(10, 10));
+      //frame.setLayout(new GridLayout(10, 10));
       frame.setSize(800,800);
       frame.setResizable(false);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.setTitle("BattleJava!!!");
       frame.setLocationRelativeTo(null);
-      frame.setVisible(true);
-      OceanPanel panel = new OceanPanel(10, 10);
+      OceanPanel panel = new OceanPanel(50);
       panel.setSize( 400, 800 );
       frame.add(panel);
+      frame.setVisible(true);
    }
 }
