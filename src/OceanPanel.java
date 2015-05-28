1ef4eb182be2a798582e24d08082d7c61a8bd2cb
Joe D'Ambrosia <joe.jokoo@gmail.com>
2015-05-27 22:12:12 -0700

    The OceanPanel and battleJavaTester now create a JFrame with 2 JPannels that contain JButtons.

--- src/OceanPanel.java
+++ src/OceanPanel.java
@@ -1,25 +1,38 @@
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.util.*;
 
-
-public class OceanPanel extends JPanel implements ActionListener{
+public class OceanPanel implements ActionListener{
    private int sizeX;
    private int sizeY;
+   GridLayout grid;
+   JPanel panel;   
   
    public OceanPanel(int sizeX, int sizeY){
       this.sizeY = sizeY;
       this.sizeX = sizeX;
+      this.grid = new GridLayout(10,10);
+      this.panel = new JPanel(grid);
+      
       drawBoard();
    }
 
    private void drawBoard(){
-       for(int i = 0; i<sizeY; i++){
-          for(int j =0; j<sizeX; j++){
-            super.add(new JButton("1"));
-          }
-       }
+      Icon hitIcon = new ImageIcon("hit.gif");
+      
+      for(int i = 0; i<sizeY; i++){
+         for(int j =0; j<sizeX; j++){
+            JButton btn = new JButton(hitIcon);
+            btn.setPreferredSize(new Dimension(20, 20));
+            panel.add(btn);
+         }
+      }
       // g2d.setColor(Color.BLACK);
     // g2d.fillRect(0,0,sizeX,sizeX);
     // g2d.setColor(Color.BLUE);
@@ -30,6 +43,10 @@ public class OceanPanel extends JPanel implements ActionListener{
    
     //   }
     // }
+   }
+   
+   public JPanel getPanel(){
+      return this.panel;
    }
    public void actionPerformed(ActionEvent event){
    
