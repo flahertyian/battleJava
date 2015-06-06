31e268fc262966c642eacd6e3ce27de6e1bef481
Ian Flaherty <ianrocket@gmail.com>
2015-06-06 11:52:37 -0700

    added comments to point and added methods in both playerPanel and point to add functionality with brains code when we eventually decide to connect them

--- src/Point.java
+++ src/Point.java
@@ -4,13 +4,15 @@ import javax.swing.ImageIcon;
 
 //worst name for a JButton ever lol my b 0_0
 public class Point extends JButton{
-  int x;
-  int y;
-  ImageIcon hit;
-  ImageIcon miss;
-  ImageIcon ship;
-  ImageIcon background;
-  Dimension dimension;
+  private int x;
+  private int y;
+  private ImageIcon hit;
+  private ImageIcon miss;
+  private ImageIcon ship;
+  private ImageIcon background;
+  private Dimension dimension;
+  private boolean isShip;
+
 
   public Point(int x, int y){
     background = new ImageIcon("../resource/battleJavaBase.gif");
@@ -21,23 +23,36 @@ public class Point extends JButton{
     hit = new ImageIcon("../resource/battleJavaHit.gif");
     miss = new ImageIcon("../resource/battleJavaMiss.gif");
     dimension = new Dimension(x,y);
-
+    isShip = false;
   }
 
+  //sets the icon of the Point to a hit
   public void setHit(){
     super.setIcon(hit);
   }
+
+  //sets the icon of the point to a miss
   public void setMiss(){
     super.setIcon(miss);
   }
+
+  //sets the icon of the point to the original color
   public void setBackground(){
     super.setIcon(background);
   }
 
+  //both sets the icon to the color of a ship but also sets an inner boolean to true;
   public void setShip(){
     super.setIcon(ship);
+    isShip = true;
   }
 
+  //returns weather the point is a Ship or not
+  public boolean isShip(){
+   return isShip;
+  }
+
+  //returns the dimension inside the pannel of the point
   public Dimension getDimension(){
     return dimension;
   }
