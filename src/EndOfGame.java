7e7e244f4216928fe6461a2084e2b4e6267e4a41
Ian Flaherty <ianrocket@gmail.com>
2015-06-08 23:57:30 -0700

    made it compile idk if it works

--- src/EndOfGame.java
+++ src/EndOfGame.java
@@ -1,12 +1,29 @@
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.Graphics;
+import java.io.File;
+import java.awt.image.BufferedImage;
+
+
 public class EndOfGame extends JFrame{
+      BufferedImage winScreen;
+      BufferedImage loseScreen;
 
 
   public EndOfGame(boolean winOrLoss){
+    super.setSize(800,800);
+    super.setResizable(false);
+    super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    super.setTitle("Game Over");
+    super.setLocationRelativeTo(null);
     if(winOrLoss){
       winScreen();
     }else{
       loseScreen();
     }
+    super.setVisible(true);
   }
 
    public void winScreen(){
@@ -18,7 +35,7 @@ public class EndOfGame extends JFrame{
        }
        Graphics g = winScreen.getGraphics();
        g.drawImage(winScreen,0,0,this);
-       end.paintComponent(g);
+       end.paint(g);
        super.add(end);
    }
 
@@ -31,7 +48,7 @@ public class EndOfGame extends JFrame{
        }
        Graphics g = winScreen.getGraphics();
        g.drawImage(winScreen,0,0,this);
-       end.paintComponent(g);
+       end.paint(g);
        super.add(end);
 
    }
